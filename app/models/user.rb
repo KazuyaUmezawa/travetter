738ca127d39c5eb2df413fd60b73ca0c1afb9a0f
KazuyaUmezawa <KazuyaUmezawa1024@gmail.com>
2020-07-17 01:06:51 +0900

    userモデルのアソシエーションとバリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #アソシエーション
+  has_many :schedules, dependent: :destroy
+  has_many :airticles, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  #必須項目(空欄禁止)
+  validates :nickname, presence: true
 end
